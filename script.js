f8aeab5d3adbc959461a6b5c2bb2e17d78418d9f
Waqar Ahmed <blackpanthergaming426@gmail.com>
2025-12-24 04:02:19 +1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let mode = document.getElementById("mode")
+let body =document.querySelector("body")
+let currmode ="light"
+mode.addEventListener('click', () => {
+    if (currmode==="light") {
+        currmode =  "dark";
+        body.classList.remove("light")
+        body.classList.add("dark")
+    }
+    else{
+        currmode="light"
+        body.classList.remove("dark")
+        body.classList.add("light")
+    }
+    
+    
+    console.log(currmode );
+    
+})
+
+
+    
